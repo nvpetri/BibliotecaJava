2bfc4496961230d325b35e20c6124b0eb66017ec
unknown <nicolaspetri18@gmail.com>
2023-09-02 20:57:47 -0300

    teste

--- src/br/senai/sp/jandira/model/Menu.java
+++ src/br/senai/sp/jandira/model/Menu.java
@@ -18,8 +18,10 @@ public class Menu {
 			System.out.println("******* Escolha uma das opções: ***********");
 			System.out.println("********************* [1] - Cadastrar Usuário");
 			System.out.println("********************* [2] - Cadastrar Livro");
-			System.out.println("********************* [3] - Livros Emprestados");
-			System.out.println("********************* [4] - Sair");
+			System.out.println("********************* [3] - Usuários Cadastrados");
+			System.out.println("********************* [4] - Livros Cadastrados");
+			System.out.println("********************* [5] - Livros Emprestados");
+			System.out.println("********************* [6] - Sair");
 			System.out.println("-------------------------------------------");
 			System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/");
 			
@@ -36,6 +38,13 @@ public class Menu {
 					System.out.println("Feature in development");
 					break;
 				case 4:
+					System.out.println("Feature in development");
+					break;
+				case 5:
+					System.out.println("Feature in development");
+					break;
+				case 6:
+					System.out.println("Tchauuu!!");
 					continuar = false;
 					break;
 				default:
